1856fe253d2661bfa67e31601ea50219f734a54e
mgamanunes <mgamanunes@gmail.com>
2019-04-24 11:17:51 +0200

    Added a method to Question schema in polls model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+
+import datetime
 
 
 class Question(models.Model):
@@ -6,6 +9,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+    	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
