d1b2b15c8a214318d8dc6c5fc9ec67cd6f0baa61
mgamanunes <mgamanunes@gmail.com>
2019-04-24 18:10:45 +0200

    added custom field to model

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInChoiceline(admin.TabularInline):
-    model =
+class ChoiceInline(admin.TabularInline):
+    model = Choice
     extra = 3
 
 
@@ -14,7 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ('question_text', 'pub_date', 'was_published_recently', 'the_question_type')
     search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,6 +6,8 @@ import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    question_type = models.CharField(max_length=50, default="Global")
+
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
@@ -17,6 +19,15 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+
+
+    def the_question_type(self):
+        return self.question_type
+
+    the_question_type.admin_order_field = 'question_type'
+    the_question_type.short_description = 'Question type'
+
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
